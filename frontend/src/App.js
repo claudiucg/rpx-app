be129367d78d3a6d284b95d644ce0db8ae301958
Claudiu Ciobanu <claudiu.ciobanu@romprix.ro>
2020-12-14 15:13:03 +0200

    commit to main branch

---
+++ frontend/src/App.js
@@ -0,0 +1,64 @@
+
+import React from 'react';
+import data from './data';
+ 
+function App() {
+  return ( 
+    <div className="grid-container">
+            <header className="row">
+                <div>
+                    <a className="brand" href="index.html">amazona</a> 
+                </div>
+                <div>
+                    <a href="cart.html">Cart</a>
+                    <a href="signin.html">Sign In</a>
+                </div>
+            </header>
+            <main>
+                <div className="row center">
+                    {
+                        data.products.map(product => (
+                        
+                    <div key={product._id} className="card">
+                        <a href={`/product/${product._id}`}>
+                            <img className="medium" src="./images/p1.jpg" alt="product" />
+                        </a>
+                        <div className="card-body">
+                            <a href="product.html">
+                                <h2>{product.name}</h2>
+                            </a>
+                            <div className="rating">
+                                <span>
+                                    {' '}
+                                    <i className="fa fa-star"></i>{' '}
+                                </span>
+                                <span>
+                                    {' '}
+                                    <i className="fa fa-star"></i>{' '}
+                                </span>
+                                <span>
+                                    {' '}
+                                    <i className="fa fa-star"></i>{' '}
+                                </span>
+                                <span>
+                                    {' '}
+                                    <i className="fa fa-star"></i>{' '}
+                                </span>
+                                <span>
+                                    {' '}
+                                    <i className="fa fa-star"></i>{' '}
+                                </span>
+                            </div>
+                            <div className="price">${product.price}</div>
+                        </div>
+                    </div>
+                    ))
+                }
+                </div>
+            </main>
+            <footer className="row center">All rights reserved</footer>
+        </div>
+  );
+}
+
+export default App;
